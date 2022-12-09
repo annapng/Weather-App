7745a4877bae2702f5fc6d99378148f53059a91e
annapng <64225221+annapng@users.noreply.github.com>
2022-12-09 15:52:18 -0700

    Time to Deploy!!

--- README.md
+++ README.md
@@ -1,17 +1,34 @@
 # Weather App
 
+## Description
+This app takes data from the Open Weather API and allows you to search the name of any city around the world. You will receive what the current weather is like outside, as well as for the next five days after that point as well.
+
+There are lots of improvements I'd like to make over time. I'd like to fix the text formatting of the dates and times, which is possible with Moment.js. I'd like to make the styling of the page a lot more appealing. I would possibly like to include more data, or data for several weeks out if possible. 
+
+I feel that this project, along with Project 1, really helped me feel more confident about my coding abilities. Woo!
+
 ## User Story
 AS A traveler  
 I WANT to see the weather outlook for multiple cities  
 SO THAT I can plan a trip accordingly
 
 ## Acceptance Criteria
-[ ] GIVEN a weather dashboard with form inputs
-[ ] WHEN I search for a city
-[ ] THEN I am presented with current and future conditions for that city and that city is added to the search history
-[ ] WHEN I view current weather conditions for that city
-[ ] THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-[ ] WHEN I view future weather conditions for that city
-[ ] THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-[ ] WHEN I click on a city in the search history
-[ ] THEN I am again presented with current and future conditions for that city
+[x] GIVEN a weather dashboard with form inputs  
+[x] WHEN I search for a city  
+[x] THEN I am presented with current and future conditions for that city and that city is added to the search history  
+[x] WHEN I view current weather conditions for that city  
+[x] THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed  
+[x] WHEN I view future weather conditions for that city  
+[x] THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity  
+[x] WHEN I click on a city in the search history  
+[x] THEN I am again presented with current and future conditions for that city  
+ 
+## Sources
+To get both current and future weather data, I used [OpenWeather API](https://openweathermap.org/api).
+
+## Screenshot
+![Screenshot of Weather App](assets\Screenshot.png)
+
+## Links 
+Repo: 
+Deployed App:
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,3 @@
-// https://openweathermap.org/forecast5
-
-
 var APIkey = "6bce6205a22e9e00bee095bc59def9ca";
 var response;
 var weatherSearch = document.getElementById("weatherSearch");
@@ -14,25 +11,8 @@ var div3 = document.getElementById("futureDiv3");
 var div4 = document.getElementById("futureDiv4");
 var div5 = document.getElementById("futureDiv5");
 
-/*
-function clearDivs(){
-document.getElementById("searchBtn").addEventListener("click", function() {
-    document.getElementById("mainDiv").innerHTML = "";
 
-
-})}
-*/
-//var city = document.getElementById("textboxSearch").onSubmit;
-//var cityValue = textboxSearch.value;
-//console.log(city);
-
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// https://www.geeksforgeeks.org/how-to-insert-a-javascript-variable-inside-href-attribute/
-
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=6bce6205a22e9e00bee095bc59def9ca
-
-
-function search() {
+function search() { // Get Data from searchbox
     event.preventDefault();
 var cityValue = textboxSearch.value;
 console.log(cityValue);
@@ -40,65 +20,58 @@ weatherFunction(cityValue);
 }
 
 
-function weatherFunction(cityValue) {
-  //  event.preventDefault();
-   // clearDivs();
-//    var cityValue = textboxSearch.value;
+function weatherFunction(cityValue) { // Gather information from OpenWeather API
     console.log(cityValue);
     cityDivs(cityValue);
 
     var url = "https://api.openweathermap.org/data/2.5/weather?q="+cityValue+"&limit=1&units=imperial&appid="+APIkey;
     console.log(url);
     let response = fetch(url)
-         .then(response => response.text())
-         .then((data) => { data = JSON.parse(data);
-                           console.log(data);
+            .then(response => response.text())
+            .then((data) => { data = JSON.parse(data);
+                            console.log(data);
                         
-                          currentDiv(data);});
+                            currentDiv(data);});
 
     console.log(response);
 
     var futureUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+cityValue+"&limit=1&cnt=40&units=imperial&appid="+APIkey+"&cnt=5&list.dt=12:00:00";
-    let futureResponse = fetch(futureUrl)
-    .then(futureResponse => futureResponse.text())
-    .then((futureData) => {
-        futureData = JSON.parse(futureData);
-        console.log(futureData);
-        futureDivs(futureData);
+    fetch(futureUrl)
+        .then(futureResponse => futureResponse.text())
+        .then((futureData) => {
+            futureData = JSON.parse(futureData);
+            console.log(futureData);
+            futureDivs(futureData);
+        });
+}
+
+
+function weatherFunction2(cityValue) { // 2nd Weather Function, to send values from the previous searched Divs to the right area 
+    console.log(cityValue);
+
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+cityValue+"&limit=1&units=imperial&appid="+APIkey;
+    console.log(url);
+    let response = fetch(url)
+        .then(response => response.text())
+        .then((data) => { data = JSON.parse(data);
+                            console.log(data);
+                        
+                        currentDiv(data);});
+
+    console.log(response);
+
+    var futureUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+cityValue+"&limit=1&cnt=40&units=imperial&appid="+APIkey+"&cnt=5&list.dt=12:00:00";
+    fetch(futureUrl)
+        .then(futureResponse => futureResponse.text())
+        .then((futureData) => {
+            futureData = JSON.parse(futureData);
+            console.log(futureData);
+            futureDivs2(futureData);
     });
 }
 
 
-
-function weatherFunction2(cityValue) {
-    //  event.preventDefault();
-     // clearDivs();
-  //    var cityValue = textboxSearch.value;
-      console.log(cityValue);
-  
-      var url = "https://api.openweathermap.org/data/2.5/weather?q="+cityValue+"&limit=1&units=imperial&appid="+APIkey;
-      console.log(url);
-      let response = fetch(url)
-           .then(response => response.text())
-           .then((data) => { data = JSON.parse(data);
-                             console.log(data);
-                          
-                            currentDiv(data);});
-  
-      console.log(response);
-  
-      var futureUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+cityValue+"&limit=1&cnt=40&units=imperial&appid="+APIkey+"&cnt=5&list.dt=12:00:00";
-      let futureResponse = fetch(futureUrl)
-      .then(futureResponse => futureResponse.text())
-      .then((futureData) => {
-          futureData = JSON.parse(futureData);
-          console.log(futureData);
-          futureDivs2(futureData);
-      });
-  }
-
-
-function currentDiv(data) {
+function currentDiv(data) { // formatting for the Current Day div
 
     var city = data.name;
     console.log(city);
@@ -111,20 +84,17 @@ function currentDiv(data) {
     document.getElementById("todaysIcon").innerHTML = "<img src='" + iconURL + "' />";
 
     var temp = data.main.temp;
-    document.getElementById("todaysTemp").innerHTML = temp;
-
+    document.getElementById("todaysTemp").innerHTML = "Temperature: " + temp + "° F";
 
     var wind = data.wind.speed;
-    document.getElementById("todaysWind").innerHTML = wind;
+    document.getElementById("todaysWind").innerHTML = "Wind Speed: " + wind + " mph";
 
     var humidity = data.main.humidity;
-    document.getElementById("todaysHumidity").innerHTML = humidity;
-
+    document.getElementById("todaysHumidity").innerHTML = "Humidity: " + humidity + " g/m3";
 }
 
 
-function futureDivs (futureData){
-
+function futureDivs (futureData){ // Formatting for the Future forecast Divs
 
     document.getElementsByClassName("futureForecastDivs").innerHTML = " ";
     div1.innerHTML = " ";
@@ -135,11 +105,11 @@ function futureDivs (futureData){
 
 
     var humidity1 = futureData.list[3].main.humidity;
-        div1.insertAdjacentHTML('afterBegin', "<br>" + humidity1 + "<p>");
+        div1.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity1 + " g/m3" + "<p>");
     var wind1 = futureData.list[3].wind.speed;
-        div1.insertAdjacentHTML('afterBegin', "<br>" + wind1);
+        div1.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind1 + " mph");
     var temp1 = futureData.list[3].main.temp;
-        div1.insertAdjacentHTML('afterBegin', "<br>" + temp1);
+        div1.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp1 + "° F");
     var icon1 = futureData.list[3].weather[0].icon;
     var iconURL1 = "http://openweathermap.org/img/wn/" + icon1 +"@2x.png";
         div1.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL1 + "' />");
@@ -147,11 +117,11 @@ function futureDivs (futureData){
         div1.insertAdjacentText('afterBegin', date1);
 
     var humidity2 = futureData.list[11].main.humidity;
-        div2.insertAdjacentHTML('afterBegin', "<br>" + humidity2 + "<p>");
+        div2.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity2 + " g/m3" + "<p>");
     var wind2 = futureData.list[11].wind.speed;
-        div2.insertAdjacentHTML('afterBegin', "<br>" + wind2);
+        div2.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind2 + " mph");
     var temp2 = futureData.list[11].main.temp;
-        div2.insertAdjacentHTML('afterBegin', "<br>" + temp2);
+        div2.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp2 + "° F");
     var icon2 = futureData.list[11].weather[0].icon;
     var iconURL2 = "http://openweathermap.org/img/wn/" + icon2 +"@2x.png";
         div2.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL2 + "' />");
@@ -159,11 +129,11 @@ function futureDivs (futureData){
         div2.insertAdjacentText('afterBegin', date2);
 
     var humidity3 = futureData.list[19].main.humidity;
-        div3.insertAdjacentHTML('afterBegin', "<br>" + humidity3 + "<p>");
+        div3.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity3 + " g/m3" + "<p>");
     var wind3 = futureData.list[19].wind.speed;
-        div3.insertAdjacentHTML('afterBegin', "<br>" + wind3);
+        div3.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind3 + " mph");
     var temp3 = futureData.list[19].main.temp;
-        div3.insertAdjacentHTML('afterBegin', "<br>" + temp3);
+        div3.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp3 + "° F");
     var icon3 = futureData.list[19].weather[0].icon;
     var iconURL3 = "http://openweathermap.org/img/wn/" + icon3 +"@2x.png";
             div3.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL3 + "' />");
@@ -171,11 +141,11 @@ function futureDivs (futureData){
         div3.insertAdjacentText('afterBegin', date3);
 
     var humidity4 = futureData.list[27].main.humidity;
-        div4.insertAdjacentHTML('afterBegin', "<br>" + humidity4 + "<p>");
+        div4.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity4 + " g/m3" + "<p>");
     var wind4 = futureData.list[27].wind.speed;
-        div4.insertAdjacentHTML('afterBegin', "<br>" + wind4);
+        div4.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind4 + " mph");
     var temp4 = futureData.list[27].main.temp;
-        div4.insertAdjacentHTML('afterBegin', "<br>" + temp4);
+        div4.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp4 + "° F");
     var icon4 = futureData.list[27].weather[0].icon;
     var iconURL4 = "http://openweathermap.org/img/wn/" + icon4 +"@2x.png";
             div4.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL4 + "' />");
@@ -183,11 +153,11 @@ function futureDivs (futureData){
         div4.insertAdjacentText('afterBegin', date4);
 
     var humidity5 = futureData.list[35].main.humidity;
-        div5.insertAdjacentHTML('afterBegin', "<br>" + humidity5 + "<p>");
+        div5.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity5 + " g/m3" + "<p>");
     var wind5 = futureData.list[35].wind.speed;
-        div5.insertAdjacentHTML('afterBegin', "<br>" + wind5);
+        div5.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind5 + " mph");
     var temp5 = futureData.list[35].main.temp;
-        div5.insertAdjacentHTML('afterBegin', "<br>" + temp5);
+        div5.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp5 + "° F");
     var icon5 = futureData.list[35].weather[0].icon;
     var iconURL5 = "http://openweathermap.org/img/wn/" + icon5 +"@2x.png";
             div5.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL5 + "' />");
@@ -196,7 +166,7 @@ function futureDivs (futureData){
 
 }
 
-function futureDivs2 (futureData){
+function futureDivs2 (futureData){ // 2nd function, for the recall
 
     document.getElementsByClassName("futureForecastDivs").innerHTML = " ";
     div1.innerHTML = " ";
@@ -207,11 +177,11 @@ function futureDivs2 (futureData){
 
 
     var humidity1 = futureData.list[3].main.humidity;
-        div1.insertAdjacentHTML('afterBegin', "<br>" + humidity1 + "<p>");
+        div1.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity1 + " g/m3" + "<p>");
     var wind1 = futureData.list[3].wind.speed;
-        div1.insertAdjacentHTML('afterBegin', "<br>" + wind1);
+        div1.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind1 + " mph");
     var temp1 = futureData.list[3].main.temp;
-        div1.insertAdjacentHTML('afterBegin', "<br>" + temp1);
+        div1.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp1 + "° F");
     var icon1 = futureData.list[3].weather[0].icon;
     var iconURL1 = "http://openweathermap.org/img/wn/" + icon1 +"@2x.png";
         div1.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL1 + "' />");
@@ -219,11 +189,11 @@ function futureDivs2 (futureData){
         div1.insertAdjacentText('afterBegin', date1);
 
     var humidity2 = futureData.list[11].main.humidity;
-        div2.insertAdjacentHTML('afterBegin', "<br>" + humidity2 + "<p>");
+        div2.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity2 + " g/m3" + "<p>");
     var wind2 = futureData.list[11].wind.speed;
-        div2.insertAdjacentHTML('afterBegin', "<br>" + wind2);
+        div2.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind2 + " mph");
     var temp2 = futureData.list[11].main.temp;
-        div2.insertAdjacentHTML('afterBegin', "<br>" + temp2);
+        div2.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp2 + "° F");
     var icon2 = futureData.list[11].weather[0].icon;
     var iconURL2 = "http://openweathermap.org/img/wn/" + icon2 +"@2x.png";
         div2.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL2 + "' />");
@@ -231,11 +201,11 @@ function futureDivs2 (futureData){
         div2.insertAdjacentText('afterBegin', date2);
 
     var humidity3 = futureData.list[19].main.humidity;
-        div3.insertAdjacentHTML('afterBegin', "<br>" + humidity3 + "<p>");
+        div3.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity3 + " g/m3" + "<p>");
     var wind3 = futureData.list[19].wind.speed;
-        div3.insertAdjacentHTML('afterBegin', "<br>" + wind3);
+        div3.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind3 + " mph");
     var temp3 = futureData.list[19].main.temp;
-        div3.insertAdjacentHTML('afterBegin', "<br>" + temp3);
+        div3.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp3 + "° F");
     var icon3 = futureData.list[19].weather[0].icon;
     var iconURL3 = "http://openweathermap.org/img/wn/" + icon3 +"@2x.png";
             div3.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL3 + "' />");
@@ -243,11 +213,11 @@ function futureDivs2 (futureData){
         div3.insertAdjacentText('afterBegin', date3);
 
     var humidity4 = futureData.list[27].main.humidity;
-        div4.insertAdjacentHTML('afterBegin', "<br>" + humidity4 + "<p>");
+        div4.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity4 + " g/m3" + "<p>");
     var wind4 = futureData.list[27].wind.speed;
-        div4.insertAdjacentHTML('afterBegin', "<br>" + wind4);
+        div4.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind4 + " mph");
     var temp4 = futureData.list[27].main.temp;
-        div4.insertAdjacentHTML('afterBegin', "<br>" + temp4);
+        div4.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp4 + "° F");
     var icon4 = futureData.list[27].weather[0].icon;
     var iconURL4 = "http://openweathermap.org/img/wn/" + icon4 +"@2x.png";
             div4.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL4 + "' />");
@@ -255,11 +225,11 @@ function futureDivs2 (futureData){
         div4.insertAdjacentText('afterBegin', date4);
 
     var humidity5 = futureData.list[35].main.humidity;
-        div5.insertAdjacentHTML('afterBegin', "<br>" + humidity5 + "<p>");
+        div5.insertAdjacentHTML('afterBegin', "<br>" + "Humidity: " + humidity5 + " g/m3" + "<p>");
     var wind5 = futureData.list[35].wind.speed;
-        div5.insertAdjacentHTML('afterBegin', "<br>" + wind5);
+        div5.insertAdjacentHTML('afterBegin', "<br>" + "Wind Speed: " + wind5 + " mph");
     var temp5 = futureData.list[35].main.temp;
-        div5.insertAdjacentHTML('afterBegin', "<br>" + temp5);
+        div5.insertAdjacentHTML('afterBegin', "<br>" + "Temperature: " + temp5 + "° F");
     var icon5 = futureData.list[35].weather[0].icon;
     var iconURL5 = "http://openweathermap.org/img/wn/" + icon5 +"@2x.png";
             div5.insertAdjacentHTML('afterBegin', "<br><img src='" + iconURL5 + "' />");
@@ -268,13 +238,13 @@ function futureDivs2 (futureData){
 
 }
 
-function cityDivs (cityValue) {
+function cityDivs (cityValue) { // Creating the buttons to take you to see past data
     window.localStorage.setItem(cityValue, JSON.stringify(cityValue));
     prevSearchedDiv.insertAdjacentHTML('afterbegin', '<button class="prevCityDiv" class="' + cityValue + ' ">' + cityValue + '</button><p>');
 }
 
 
-window.onload = function() {
+window.onload = function() { // On page load, create the previous searched buttons
     Object.keys(localStorage).forEach(function(key){
         console.log(localStorage.getItem(key));
         prevSearchedDiv.insertAdjacentHTML('afterbegin', '<button class="prevCityDiv" class="' + key + ' ">' + key + '</button><p>');
@@ -284,7 +254,7 @@ window.onload = function() {
 }
 
 
-function buttonLinks() {
+function buttonLinks() { // Creates the links from clicking a previously searched button
 
 for (const prevCityButt of prevCityDiv){
     prevCityButt.addEventListener("click", function(){
